8c8ac73d62f5da9fc368078fe8ffac779eb3c35d
Caleb Lee <caleb.gwl@gmail.com>
2017-08-29 13:10:00 -0500

    Refactor testimonials, add three more

---
+++ app/controllers/about.js
@@ -0,0 +1,36 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  items: [
+    {
+      src: '/images/testimonials/karen.jpg',
+      name: 'Karen Adams',
+      quote: 'Prom was a success!',
+    },
+    {
+      src: '/images/testimonials/rasheeda.jpg',
+      name: 'Rasheeda Cross',
+      quote: 'Thank you so much for having what we wanted, money well spent.',
+    },
+    {
+      src: '/images/testimonials/ollie-green.jpg',
+      name: 'Ollie Green',
+      quote: 'Thank you Bella Devine for my beautiful dress to celebrate my 50th birthday. I searched all over and found the perfect dress, perfect color and the right plus size. I am a very happy Customer.',
+    },
+    {
+      src: '/images/testimonials/macy.jpg',
+      name: 'Macy Beth',
+      quote: 'Thank you so much for helping me find the perfect dress.',
+    },
+    {
+      src: '/images/testimonials/michelle.jpg',
+      name: 'Rasheeda Cross',
+      quote: 'Thank You so much for Addie\'s Princess Dress. She won Miss Strawberry, Best Personality and Best Hair today! And she had a beautiful dress to wear.',
+    },
+    {
+      src: '/images/testimonials/diana.jpg',
+      name: 'Diana Shields',
+      quote: 'I just wanted to say thank you for being a part of making my daughter\'s prom special!',
+    },
+  ],
+});

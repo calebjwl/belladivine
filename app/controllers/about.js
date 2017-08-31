373c365ccb541601f12c983e2e94bb0b78436eb2
Caleb Lee <caleb.gwl@gmail.com>
2017-08-31 14:46:48 -0500

    Minor fixes to testimonial

--- app/controllers/about.js
+++ app/controllers/about.js
@@ -3,19 +3,9 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   items: [
     {
-      src: '/images/testimonials/karen.jpg',
-      name: 'Karen Adams',
-      quote: 'Prom was a success!',
-    },
-    {
-      src: '/images/testimonials/rasheeda.jpg',
-      name: 'Rasheeda Cross',
-      quote: 'Thank you so much for having what we wanted, money well spent.',
-    },
-    {
       src: '/images/testimonials/ollie-green.jpg',
       name: 'Ollie Green',
-      quote: 'Thank you Bella Devine for my beautiful dress to celebrate my 50th birthday. I searched all over and found the perfect dress, perfect color and the right plus size. I am a very happy Customer.',
+      quote: 'Thank you Bella Devine for my beautiful dress to celebrate my 50th birthday. I searched all over and found the perfect dress, perfect color and the right plus size. I am a very happy customer.',
     },
     {
       src: '/images/testimonials/macy.jpg',
@@ -31,6 +21,16 @@ export default Ember.Controller.extend({
       src: '/images/testimonials/diana.jpg',
       name: 'Diana Shields',
       quote: 'Thank you for being a part of making my daughter\'s prom special!',
+    },
+    {
+      src: '/images/testimonials/rasheeda.jpg',
+      name: 'Rasheeda Cross',
+      quote: 'Thank you so much for having what we wanted, money well spent.',
+    },
+    {
+      src: '/images/testimonials/karen.jpg',
+      name: 'Karen Adams',
+      quote: 'Prom was a success!',
     },
   ],
 });
